3cc7ce4dcc00d508a87eaf0ffbb780400633839e
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-03-03 11:40:26 +0600

    name & features added

--- js/ai.js
+++ js/ai.js
@@ -26,10 +26,25 @@ ais.forEach(ai => {
     <div class="card h-100">
     <img src="${ai.image}" class="card-img-top" alt="...">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      <h5 class="card-title">Features</h5>
+      <ol type="1">
+      <li>
+      ${ai.features[0]      }
+      </li>
+      <li>
+      ${ai.features[1]      }
+      </li>
+      <li>
+      ${ai.features[2]      }
+      </li>
+      </ol>
+     
+        
+      
     </div>
     <div class="card-footer">
+    <h5 class="card-title"> ${ai.name     }</h5>
+
       <small class="text-muted">Last updated 3 mins ago</small>
     </div>
   </div>
