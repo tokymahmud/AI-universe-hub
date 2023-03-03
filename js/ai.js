b51e637a8a819f56826fe89d8132ffa34553fb8c
toky.mahmud <toky.mahmud.tm@gmail.com>
2023-03-04 00:39:40 +0600

    spinner and others

--- js/ai.js
+++ js/ai.js
@@ -12,11 +12,17 @@ loadais();
 
     
 const displayai = ais=>{
-    // console.log(ais);
+    console.log(ais);
     const aiscontainer = document.getElementById('ai-Container');
-// fordisplaying only the search one
+
 //    aicontainer.innerHTML='';
 
+// slice part
+if(ais.length >5){
+    ais = ais.slice(0, 5);
+    const showAll = document.getElementById('show-all');
+    showAll.classList.remove('d-none');
+}
 
 ais.forEach(ai => {
     console.log(ai)
@@ -50,13 +56,53 @@ ais.forEach(ai => {
     </div>
 
     <div class="gap-5 p-4 position-relative ">
-    <i class="fa-solid fa-arrow-right position-absolute top-0 start-100 translate-middle fs-1 text-danger rounded-circle bg-danger bg-opacity-25 align-items-center justify-content-center mx-5 mt-4"></i>
+    <button type="button" class="btn " data-bs-toggle="modal" data-bs-target="#arrowModal">
+    <i class="fa-solid fa-arrow-right position-absolute top-0 start-100 translate-middle fs-1 text-danger rounded-circle bg-danger bg-opacity-25 align-items-center justify-content-center mx-5 mt-4 "> </i>
+    </button>
+
+   
+    
     </div>
   </div>
 `
 
 
 aiscontainer.appendChild(aidiv);
+
 });
+toogleSpinner(false);
+
    
 }
+
+tooglespinner(true);
+
+const tooglespinner = isLoading =>{
+    const loaderSection = document.getElementById('loader');
+    if(isLoading){
+        loaderSection.classList.remove('d-none')
+    }
+    else{
+        loaderSection.classList.add('d-none')
+
+
+    }
+} 
+toogleSpinner(false);
+
+// showall 2nd part
+
+document.getElementById('btn-show-all').addEventListener('click', function(){
+    
+    tooglespinner(true);
+})
+
+// const loadaisdetail = async id =>{
+//     const url = ` https://openapi.programming-hero.com/api/ai/tool/${id}`;
+//     const res = await fetch(url);
+//     const data = await res.json();
+//     displaymodaldetail(data);
+// }
+// const displaymodaldetail = modal =>{
+//     console.log(modal)
+// }
